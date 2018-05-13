18539a89f80cb07836a47f3760fa89caaef9c7aa
Andrew Myhre <andrew.myhre@gmail.com>
2018-05-13 19:20:30 -0400

    adds fullscreen button

--- brick/src/brick.js
+++ brick/src/brick.js
@@ -658,6 +658,27 @@ $(document).ready(function() {
         return e.preventDefault();
         }
     });
+    function toggleFullScreen() {
+        if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+         (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+          if (document.documentElement.requestFullScreen) {  
+            document.documentElement.requestFullScreen();  
+          } else if (document.documentElement.mozRequestFullScreen) {  
+            document.documentElement.mozRequestFullScreen();  
+          } else if (document.documentElement.webkitRequestFullScreen) {  
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+          }  
+        } else {  
+          if (document.cancelFullScreen) {  
+            document.cancelFullScreen();  
+          } else if (document.mozCancelFullScreen) {  
+            document.mozCancelFullScreen();  
+          } else if (document.webkitCancelFullScreen) {  
+            document.webkitCancelFullScreen();  
+          }  
+        }  
+      }
+    $('#toggle-fullscreen').click(toggleFullScreen);
     showLC();
     });
 
