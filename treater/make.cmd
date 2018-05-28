83595c8447bac7c2e3d36347e464957162e30d76
Andrew Myhre <andrew.myhre@gmail.com>
2018-05-28 13:22:00 -0400

    delete treater project

--- treater/make.cmd
+++
@@ -1,3 +0,0 @@
-docker build -t andrewmyhre/treater-compiler -f compiler.Dockerfile .
-docker run -v %cd%:/go/src/treater andrewmyhre/treater-compiler go build
-docker build -t andrewmyhre/treater .
